38d7e3435f29b06db6243fa8c6ad1094dbc48a12
ytikhov <ahojte@gmail.com>
2023-12-02 01:15:21 +0300

    Day 01

---
+++ src/main/kotlin/com/ytikhov/Main.kt
@@ -0,0 +1,7 @@
+package com.ytikhov
+
+fun main(args: Array<String>) {
+    Day01().part1("day01/day1.txt") // right answer is 54159
+    Day01().part2("day01/day1.txt") // right answer is 53866
+
+}
